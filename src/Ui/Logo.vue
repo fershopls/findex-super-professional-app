afbf09da85e7b7864535fe3cdb54d28c7c6b4871
Fernando <shaaf4ever@gmail.com>
2023-03-30 17:46:23 -0600

    Add logo

---
+++ src/Ui/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex">
+    <div
+        class="text-7xl font-black text-8xl -tracking-wider bg-gradient-to-r from-[#344A68] to-blue-500 bg-clip-text text-transparent">
+      FinDox
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
