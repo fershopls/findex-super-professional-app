2d73f2c86d68ab7e48c0c460eff7eeab4ff2a1cf
Fernando <shaaf4ever@gmail.com>
2023-03-30 17:50:21 -0600

    Better search bar

--- src/App/Main.vue
+++ src/App/Main.vue
@@ -7,8 +7,12 @@
         Showing {{ rows.length }} deals.
       </div>
 
-      <div class="">
-        <input type="text" v-model="inputSearch" class="py-3 text-lg px-3 rounded shadow ring-0 outline-0"
+      <div class="bg-white flex pl-3 text-lg rounded shadow gap-4 items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+
+        <input type="text" v-model="inputSearch" class="ring-0 outline-0 flex-grow py-3 rounded"
                placeholder="Search">
       </div>
 
