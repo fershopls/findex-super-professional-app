c05a9854320f6a47918bd048eb387631781d2579
Fernando <shaaf4ever@gmail.com>
2023-03-31 13:54:19 -0600

    Table for documents

---
+++ src/App/table-docs.ts
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import {Column} from "@/App/Table/types";
+import {Doc} from "@/types-docs";
+import {bytesToHumanReadableSize} from "@/support";
+
+
+export const columns: Column[] = [
+    { key: 'name', label: 'Name' },
+    { key: 'directory', label: 'Directory' },
+    { key: 'size', label: 'Size' },
+    { key: 'created', label: 'Created At' },
+];
+
+export function getDocumentRows(documents: Doc[]) {
+    return documents.map(document => [
+        {
+            column: 'name',
+            value: document.name,
+        },
+        {
+            column: 'created',
+            value: dayjs(document.created).format('MMM D, YYYY'),
+        },
+        {
+            column: 'directory',
+            value: document.system_folder_name,
+        },
+        {
+            column: 'size',
+            value: bytesToHumanReadableSize(parseInt(document.size)),
+        },
+    ]);
+}
--- src/support.ts
+++ src/support.ts
@@ -24,3 +24,15 @@ export function useEscKey(callback: () => void) {
     onMounted(() => window.addEventListener('keydown', handleKeydown));
     onUnmounted(() => window.removeEventListener('keydown', handleKeydown));
 }
+
+export function bytesToHumanReadableSize(bytes: number): string {
+    if (bytes < 1024) {
+        return bytes + ' Bytes';
+    } else if (bytes >= 1024 && bytes < Math.pow(1024, 2)) {
+        return (bytes / 1024).toFixed(2) + 'KB';
+    } else if (bytes >= Math.pow(1024, 2) && bytes < Math.pow(1024, 3)) {
+        return (bytes / Math.pow(1024, 2)).toFixed(2) + 'MB';
+    } else {
+        return (bytes / Math.pow(1024, 3)).toFixed(2) + 'GB';
+    }
+}
