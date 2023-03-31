d4181f7f1da88957540dcfe4c1a9d90a754b2369
Fernando <shaaf4ever@gmail.com>
2023-03-30 19:54:12 -0600

    Add ui button component

---
+++ src/Ui/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="border bg-black rounded-lg px-2 py-3 text-white active:scale-95 transition">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
