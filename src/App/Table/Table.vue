8c56850ead03729750ac7ad5497dbaf169f1e311
Fernando <shaaf4ever@gmail.com>
2023-03-30 21:06:18 -0600

    Github link

--- src/App/Table/Table.vue
+++ src/App/Table/Table.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <div class="flex justify-end">
+    <div class="flex justify-end gap-5">
+      <a href="https://github.com/fershopls/findex-super-professional-app" target="_blank">
+        <ui-button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+          </svg>
+
+          <span class="pr-1">See GithubCode</span>
+        </ui-button>
+      </a>
       <ui-button @click="onExport()">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
              stroke="currentColor" class="w-5 h-5">
