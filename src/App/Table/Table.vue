4b4786f5aaa7e5b73518064d6fc78ab0a9e543d3
Fernando <shaaf4ever@gmail.com>
2023-03-30 18:55:03 -0600

    Use new types

--- src/App/Table/Table.vue
+++ src/App/Table/Table.vue
@@ -3,27 +3,26 @@
     <table class="w-full">
       <thead class="border-b">
       <tr>
-        <th v-for="column in columns" :key="column"
-            class="py-3 text-left px-3 font-normal text-sm text-gray-500 hover:bg-blue-100 cursor-pointer"
-            @click="onSortBy(column)"
+        <th v-for="column in columns" :key="column.key"
+            class="py-3 text-left px-3 font-normal text-xs uppercase tracking-widest text-gray-500 hover:bg-blue-100 cursor-pointer"
         >
           <div class="flex items-center gap-1">
-            <template v-if="isSortedBy(column)">
+            <template v-if="0">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                    stroke="currentColor" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
               </svg>
             </template>
 
-            {{ column }}
+            {{ column.label }}
           </div>
         </th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="row in props.rows" :key="row.id">
-        <td v-for="column in columns" :key="column" class="px-4">
-          {{ row[column] }}
+      <tr v-for="row in rows" :key="row.id" class="even:bg-gray-100">
+        <td v-for="column in columns" :key="column.key" class="px-4 py-3 text-sm">
+          {{ row.find(r => r.column === column.key)?.value }}
         </td>
       </tr>
       </tbody>
@@ -33,17 +32,17 @@
 
 <script lang="ts" setup>
 const props = defineProps<{
-  rows: any[];
+  columns: { key: string; label: string }[];
+  rows: { id: number; column: string; value: string }[];
 }>();
 
-const columns = computed(() => Object.keys(props.rows[0]));
-const sortBy = ref('');
-
-function onSortBy(column) {
-  sortBy.value = column;
-}
-
-function isSortedBy(column) {
-  return sortBy.value === column;
-}
+// const sortBy = ref('');
+//
+// function onSortBy(column) {
+//   sortBy.value = column;
+// }
+//
+// function isSortedBy(column) {
+//   return sortBy.value === column;
+// }
 </script>
