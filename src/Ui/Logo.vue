a9041beed0d454b6a42f44f996792de267550246
Fernando <shaaf4ever@gmail.com>
2023-03-31 14:00:37 -0600

    Move github button

--- src/Ui/Logo.vue
+++ src/Ui/Logo.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="flex">
+  <div class="flex items-center justify-between flex-wrap">
     <div
         class="text-7xl font-black text-8xl -tracking-wider bg-gradient-to-r from-[#344A68] to-blue-500 bg-clip-text text-transparent">
       FinDox
     </div>
+
+    <a href="https://github.com/fershopls/findex-super-professional-app" target="_blank">
+      <ui-button>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+             stroke="currentColor" class="w-5 h-5">
+          <path stroke-linecap="round" stroke-linejoin="round"
+                d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"/>
+        </svg>
+
+        <span class="pr-1">See Project Code in Github</span>
+      </ui-button>
+    </a>
   </div>
 </template>
 
